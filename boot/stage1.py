b725f4abd53d3831fa9a5e5d1055c9c465e6c818
Katharine Berry <katharine@kathar.in>
2018-02-04 22:10:51 -0800

    Add locale passthrough.

--- boot/stage1.py
+++ boot/stage1.py
@@ -7,8 +7,11 @@ CLOUDPEBBLE_WS_PROXY = 'wss://ws-proxy.cloudpebble.rebble.io/device'
 boot_api = Blueprint('boot_api', __name__)
 
 
-def patch_boot(endpoint: str, version: str):
-    boot = requests.get(f'{UPSTREAM_BOOT}/{endpoint}', params={'app_version': version}).json()
+def patch_boot(endpoint: str, locale: str=None, version: str=None):
+    boot = requests.get(f'{UPSTREAM_BOOT}/{endpoint}', params={
+        'app_version': version or '4.4',
+        'locale': locale or 'en_US'
+    }).json()
     boot['config'].get('developer', {})['ws_proxy_url'] = CLOUDPEBBLE_WS_PROXY
     boot['config']['href'] = request.base_url
     return boot
@@ -16,11 +19,13 @@ def patch_boot(endpoint: str, version: str):
 
 @boot_api.route('/ios')
 def boot_ios():
-    app_version = request.args.get('app_version', '4.3')
-    return jsonify(patch_boot('ios/v3/207/28', app_version))
+    app_version = request.args.get('app_version')
+    locale = request.args.get('locale')
+    return jsonify(patch_boot('ios/v3/207/28', locale, app_version))
 
 
 @boot_api.route('/android/v3/<int:build>')
 def boot_android(build: int):
-    app_version = request.args.get('app_version', '4.3')
-    return jsonify(patch_boot(f'android/v3/{build}', app_version))
+    app_version = request.args.get('app_version')
+    locale = request.args.get('locale')
+    return jsonify(patch_boot(f'android/v3/{build}', locale, app_version))
