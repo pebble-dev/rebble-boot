01e7d13a4124e95f5ed54d265b5e3af0b476848a
Katharine Berry <katharine@kathar.in>
2018-03-03 22:14:40 -0800

    Migrate to Lambda

--- serve_gevent.py
+++
@@ -1,8 +0,0 @@
-import os
-
-from gevent.monkey import patch_all; patch_all()
-from gevent.wsgi import WSGIServer
-from boot import app
-
-http_server = WSGIServer(('', int(os.environ.get('PORT', '5000'))), app)
-http_server.serve_forever()
