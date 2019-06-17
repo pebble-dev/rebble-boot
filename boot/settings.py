900079ed47290c589570796b20cfb9491ad25418
Joshua Wise <joshua@accelerated.tech>
2019-06-17 02:41:47 -0400

    Use http to talk to auth on prod, to save a round trip.

--- boot/settings.py
+++ boot/settings.py
@@ -1,12 +1,13 @@
 from os import environ
 
 domain_root = environ.get('DOMAIN_ROOT')
+http_protocol = environ.get('HTTP_PROTOCOL', 'https')
 
 config = {
 #    'SERVER_NAME': f"boot.{environ['DOMAIN_ROOT']}",
     'SECRET_KEY': environ['SECRET_KEY'],
     'DOMAIN_ROOT': domain_root,
-    'REBBLE_AUTH_URL': environ.get('REBBLE_AUTH_URL', f"http://auth.{domain_root}"),
+    'REBBLE_AUTH_URL': environ.get('REBBLE_AUTH_URL', f"{http_protocol}://auth.{domain_root}"),
     'APPSTORE_URL': environ.get('APPSTORE_URL', f"http://apps.{domain_root}"),
     'APPSTORE_API_URL': environ.get('APPSTORE_API_URL', f"http://appstore-api.{domain_root}"),
     'COHORTS_URL': environ.get('COHORTS_URL', f"http://cohorts.{domain_root}"),
